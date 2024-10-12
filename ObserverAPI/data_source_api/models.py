835e8a6d665917051e54cae34b5ba5f2e2bb9653
Smit Shah <smitshah24.ss@gmail.com>
2024-10-12 12:06:09 -0400

    added commented data source models context

--- ObserverAPI/data_source_api/models.py
+++ ObserverAPI/data_source_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# class DataSource:
+#     """contains general source information"""
+#     # user, created date time, source name
+
+# class DataSourceFile:
+#     """contains source file information for data-source"""
+#     #  source, filename, filepath, created date time
